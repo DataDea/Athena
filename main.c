a869eb7627c0cfc7502073f27f7b99b0604390ee
yanghai <yanghai@ablecloud.cn>
2020-06-17 01:06:02 +0800

    add list test

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include "t_list/list.h"
+#include "t_list/test_list/test_list.cpp"
+
 int main() {
+    list *ret = Test_listCreate();
+    printf("%lu", sizeof(*ret));
     return 0;
 }
--- t_list/test_list/test_list.cpp
+++ t_list/test_list/test_list.cpp
@@ -2,3 +2,8 @@
 // Created by 杨海 on 2020/6/12.
 //
 
+#include "../list.h"
+
+list *Test_listCreate() {
+    return listCreate();
+}
