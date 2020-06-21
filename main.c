081f07bad5fdb24f6a5c0d4325745d9034c18e2d
yanghai <yanghai@ablecloud.cn>
2020-06-22 00:13:32 +0800

    delete test_list

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include "t_list/list.h"
-#include "t_list/test_list/test_list.cpp"
 
 int main() {
 
-//    list *ret = Test_listCreate();
-//    printf("%lu", sizeof(*ret));
-
-    Test_listRelease();
     return 0;
 }
